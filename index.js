761874e9351e52d81384dcb54200ccc3ff90b966
emmanuelmontoyalopez24 <emmanuelprogramador24@gmail.com>
2023-12-11 10:04:52 -0500

    Aprendiendo JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+let numero = 10.1212;
+let nombre = "E,m,m,a,n,u,e,l"; //nombre.split(",");
+let verdadero = true;
+let falso = false;
+
+console.log(nombre.length);
+console.log(nombre.toLowerCase());
+console.log(nombre.toUpperCase());
+console.log(typeof nombre);
+let arrNombre = nombre.split(",");
+console.log(arrNombre);
+console.log(arrNombre[0]);
+
+console.log("--------------------------");
+
+console.log(parseInt(numero));
+console.log(numero.toFixed(2));
+console.log(parseFloat(numero));
